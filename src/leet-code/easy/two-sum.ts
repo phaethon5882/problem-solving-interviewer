d560ead8793d051af1d21d8abc171e5d1af46637
phaethon5882 <phaethon5882@naver.com>
2022-11-14 03:11:41 +0900

    허스키 테스트

--- src/leet-code/easy/two-sum.ts
+++ src/leet-code/easy/two-sum.ts
@@ -1,8 +1,6 @@
 function twoSum(nums: number[], target: number): number[] {
   const indices: number[] = [];
 
-  // key: nums[i]
-  // value: i
   const map = new Map<number, number>();
 
   for (let i = 0; i < nums.length; i++) {
